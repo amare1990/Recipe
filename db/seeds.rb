cb06d807dcda326abd2965c56c896e47f2debecc
amare1990 <amaremek@gmail.com>
2023-02-14 00:31:35 +0300

    Add database data into seeds using fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+users = []
+foods = []
+
+users.push(User.create(name: 'Amare', email: 'amare@test.com', password: '12345678'))
+users.push(User.create(name: 'Tiago', email: 'tiago.lelinski@gmail.com', password: '123456'))
+
+5.times do
+  users.push(User.create do |user|
+    user.name = Faker::FunnyName.name_with_initial
+    user.email = Faker::Internet.email
+    user.password = Faker::Internet.password
+  end
+  )
+end
+
+users.each do |user|
+  7.times do
+    foods.push(Food.create do |food|
+      food.name = Faker::Food.ingredient
+      food.measurement_unit = Faker::Food.metric_measurement
+      food.price = rand(0.1..16.0).round(2)
+      food.quantity = rand(0.1..16.0).round(2)
+      food.user_id = user.id
+    end
+    )
+  end
+end
