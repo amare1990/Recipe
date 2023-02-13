9c7a899a362502400e416285be9b9974630725b8
amare1990 <amaremek@gmail.com>
2023-02-14 00:02:40 +0300

    Add controolers, model, and views and routes for food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :users
+  resources :users do
+    resources :foods
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "users#index"
