eabe7fba136fe67c5a7a639e79040bd5bb5457f5
Tiago Marin <tiago.lelinski@gmail.com>
2023-02-13 15:51:54 -0300

    Run scaffold, nest routes, fix links, add seeds, add validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  resources :users
+  resources :users do
+    resources :recipes
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "users#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+users = []
+recipes = []
+
+users.push(User.create!(name: 'Tiago', email: 'tiago.lelinski@gmail.com', password: '123321'))
+
+5.times do
+  users.push(User.create do |user| 
+    user.name = Faker::FunnyName.name_with_initial
+    user.email = Faker::Internet.email
+    user.password = Faker::Internet.password
+  end
+  )
+end
+
+users.each do |user|
+  7.times do
+    user_recipe = Recipe.create do |recipe|
+      recipe.name = Faker::Food.dish
+      recipe.preparation_time =  rand(0.1..3.0).round(2)
+      recipe.cooking_time = rand(0.1..3.0).round(2)
+      recipe.description = Faker::Food.description
+      recipe.public = true
+      recipe.user_id = user.id
+    end
+  end
+end
+
