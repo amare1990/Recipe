552380a0878da86c6fa5ee625a2ce57a276364ea
Tiago Marin <tiago.lelinski@gmail.com>
2023-02-14 11:56:56 -0300

    add styles to match wireframe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def recipe_index
+    controller_name == 'recipes' && action_name == 'index'
+  end
+
+  def recipe_show
+    controller_name == 'recipes' && action_name == 'show'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root "users#index"
+  root "recipes#index"
 end
