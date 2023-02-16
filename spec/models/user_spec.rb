e3bc68021015efe693e3b5bd54f55adcb01ac25b
amare1990 <amaremek@gmail.com>
2023-02-16 14:42:48 +0300

    Add units for a User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+ before(:each) do
+  @user1 = User.new(name: "debie", email: "deb3@test.com", password:"1234567")
+ end
+
+  # before(@user1.save)
+
+  describe " model validation \n" do
+    it "should not be valid if the user has no name\n" do
+      @user1.name = nil
+      expect(@user1).to_not be_valid
+    end
+  end
+end
