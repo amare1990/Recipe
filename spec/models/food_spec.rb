4489e573055f40b8e09fd83622d5006efb2bb199
amare1990 <amaremek@gmail.com>
2023-02-16 13:01:17 +0300

    Update unit test examples for Food model validation

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-
-  subject { Food.new(name: 'Beyaynet', measurement_unit: 'unit', price: 100.5),
-quantity: 2}
+  before(:each) do
+    subject { Food.new(name: 'Beyaynet', measurement_unit: 'unit', price: 100.5, quantity: 2) }
+  end
 
   before { subject.save }
 
-  describe " model validations: \n" do
+  describe " model presence validations : \n" do
     it "\nname attribute value should be present" do
       subject.name = nil
       expect(subject).to_not be_valid
@@ -41,4 +41,24 @@ quantity: 2}
       subject.price = ' 2 units'
       expect(subject).to_not be_valid
     end
+  end
+
+  describe "model numerical validations: \n" do
+    it "\n The price should not be zero" do
+      subject.price = 0
+      expect(subject).not_to be_valid
+    end
+    it "\n The price should not be -ve number" do
+      subject.price = -1.5
+      expect(subject).not_to be_valid
+    end
+    it "\n The quantity should not be zero" do
+      subject.quantity = 0
+      expect(subject).not_to be_valid
+    end
+    it "\n The quantity should not be -ve number" do
+      subject.quantity = -4.5
+      expect(subject).not_to be_valid
+    end
+  end
 end
